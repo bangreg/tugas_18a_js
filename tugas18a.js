f008b6b692f2e3e39caca5e1d7e9ae3587b8f11b
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:27:23 +0700

    Tugas 18a JS
    Ambilah dua kata “long established” dalam sebuah kalimat ini : “It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.”

---
+++ tugas18a.js
@@ -0,0 +1,5 @@
+function kata(){
+  var word = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+  console.log(word.slice(8,24));
+}
+kata();
